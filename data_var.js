3822f4b83800afbda10e125603b7f60842d28faa
Afrin Z <sulthanaafrin94@gmail.com>
2023-09-11 21:52:54 -0500

    DVAssign

---
+++ data_var.js
@@ -0,0 +1,44 @@
+/*console.log(typeof "Hello, World!");
+console.log(typeof 17);
+console.log(typeof 3.14);
+console.log(typeof "17");
+console.log(typeof "3.2");
+console.log(typeof "17");
+console.log(typeof "3.2");
+console.log(42000);
+console.log(42,000);
+console.log(42, 17, 56, 34, 11, 4.35, 32);
+console.log(3.4, "hello", 45);
+console.log(Number("2345"));
+console.log(typeof Number("2345"));
+console.log(Number(17));
+console.log(Number("23bottles"));
+console.log(String(17));
+console.log(String(123.45));
+console.log(typeof String(123.45));
+console.log(1 + 1);
+console.log("1" + "1");
+console.log(2 + 3);
+console.log(2 - 3);
+console.log(2 * 3);
+console.log(2 ** 3);
+console.log(3 ** 2);
+let sum = 1 + 2;
+console.log(sum);
+let x = 1;
+x += 1;
+console.log(x);*/
+
+let nameOfTheSpaceShuttle = "determination";
+let shuttleSpeed_mph = 17500;
+let distanceToMars_km = 225000000;
+let distanceToTheMoon_km = 384400;
+let milesPerKilometer = 0.621
+console.log(milesToMars = distanceToMars_km * milesPerKilometer);
+console.log(hoursToReachMars = milesToMars / shuttleSpeed_mph);
+console.log(daysToReachMars = hoursToReachMars / 24);
+console.log("determination\twill\ttake\t332\tdays\tto\treach\tMars.");
+console.log(milesToMoon = distanceToTheMoon_km * milesPerKilometer);
+console.log(hoursToReachMoon = milesToMoon / shuttleSpeed_mph);
+console.log(daysToReachMoon = hoursToReachMoon / 24);
+console.log("determination\twill\ttake\t0.56\tdays\tto\treach\tMoon.");
