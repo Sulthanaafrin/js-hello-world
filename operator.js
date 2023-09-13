f063f9bb3c9ce92fc0a52adbb6316c4f981273dd
Afrin Z <sulthanaafrin94@gmail.com>
2023-09-12 22:19:24 -0500

    operator_ex.js

---
+++ operator.js
@@ -0,0 +1,50 @@
+let engineIndicatorLight = "redBlinking";
+let spaceSuitsOn = true;
+let shuttleCabinready = true;
+let crewStatus = spaceSuitsOn && shuttleCabinready;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+
+/*if (engineIndicatorLight === "green") {
+    console.log("engines have started");
+ } else if (engineIndicatorLight === "green blinking") {
+    console.log("engines are preparing to start");
+ } else {
+    console.log("engines are off");
+ }
+
+ if (crewStatus) {
+    console.log("Crew Ready");
+ } else {
+    console.log("Crew Not Ready");
+ }
+
+ if (computerStatusCode === 200) {
+    console.log("Please Stand By. Computer is Rebooting");
+ } else if (computerStatusCode === 400) {
+console.log("Sucess! Computer Online")
+ } else {
+    console.log("ALERT: Computer Offline");
+ }
+
+ if (shuttleSpeed > 17500) {
+    console.log("ALERT: Escape Velocity Reached!");
+ } else if (shuttleSpeed < 8000) {
+    console.log("ALERT: Cannot Maintain Orbit!");
+ } else {
+    console.log("Stable Speed");
+ } 
+
+ if (crewStatus && computerStatusCode === 200 && spaceSuitsOn) {
+    console.log("all systems go");
+ } else {
+    console.log("WARNING. Not ready");
+ }
+
+ if (!crewStatus || computerStatusCode !== 200 || !spaceSuitsOn) {
+    console.log("WARNING. Not ready");
+ } else {
+    console.log("all systems go");
+ } */
+
+ 
