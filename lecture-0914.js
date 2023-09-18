193883d038c37421ff3b5c1df85a3fcbb77071a8
Afrin Z <sulthanaafrin94@gmail.com>
2023-09-18 12:39:22 -0500

    string_ex.js

--- lecture-0914.js
+++ lecture-0914.js
@@ -5,7 +5,7 @@ console.log(5 !== "5");
 
 //console.log(expression1 && expression2);
 
-//console.log(!(expression1 && expression2));*/
+//console.log(!(expression1 && expression2));
 
 const input = require('readline-sync');
 
@@ -19,4 +19,49 @@ if (myAge < 18) {
     myPhrase ="Imagine paying for health insurance.";
 } else {
     myPhrase ="Imagine having health insurance...";
-}
+}*/
+
+let jsCreator = "Brendan Eich";
+
+console.log(jsCreator[-1]);
+console.log(jsCreator[42]);
+
+let phrase = "JavaScript rocks!";
+console.log(phrase[phrase.length - 8]);
+
+let myStr = "Index";
+
+console.log(myStr[0] === 'I');
+let firstName = "Grace";
+let lastName = "Hopper";
+
+console.log(firstName, "has", firstName.length, "characters");
+console.log(lastName, "has", lastName.length, "characters");
+
+cityName = "Vienna";
+stateName = "Virginia";
+location = cityName + ", " +  stateName;
+
+
+
+let word = "JavaScript";
+
+console.log(word.slice(1,6));
+
+let language = "JavaScript";
+language.replace('J', 'Q');
+language.slice(0,5);
+console.log(language);
+let org = "  The LaunchCode Foundation ";
+let trimmed = org.trim();
+
+console.log(trimmed);
+
+let pluralNoun = 'women' ;
+let name = 'afii';
+let verb = 'look';
+let adjective = ' beautiful';
+let color = 'green';
+
+console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
+console.log(`JavaScript provides a ${color} collection of tools - including ${adjective} syntax and ${pluralNoun} - that allows ${name} to ${verb} with strongd`);
---
+++ strings.js
@@ -0,0 +1,65 @@
+let num = 1001;
+console.log(num.length);
+console.log(String(num).length);
+
+if (String(num).includes('')){
+    console.log(String(num).length-1);
+    } else {
+    console.log(String(num).length);
+    }
+
+let num2 = 123.45;
+    
+if (String(num2).includes('.')){
+        console.log(String(num2).length-1);
+        } else {
+        console.log(String(num2).length);
+        }
+
+let num3 = 812.1994;
+
+if (String(num3).includes('.')){
+    console.log(String(num3).length-1);
+    } else {
+    console.log(String(num3).length);
+    }
+
+
+
+let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT ";
+
+console.log(dna.trim());
+console.log(dna.toUpperCase());
+console.log(dna.replace('GCT', 'AGG'));
+        
+if(dna.indexOf('cAt')){
+console.log("cAt found");
+} else {
+    console.log("cAt NOT found");
+}
+
+console.log(dna.slice(16,19));
+
+console.log(dna.length);
+
+console.log(`The DNA strand is ${53} character long`);
+
+console.log(`${dna.slice(18,20).toUpperCase()}o ${dna.slice(dna.indexOf('cAt'),dna.indexOf('cAt')+3).toLowerCase()}`);
+
+
+let str = 'JavaScript'
+console.log(str.slice(0,1) + str.slice(4,5));
+
+console.log(str.replace('JavaScript', 'JS'));
+
+let initial = 'JS';
+console.log(`The abbreviation for ${str} is ${initial}`);
+
+let language = 'English';
+let shortL = 'Eng';
+console.log(`The abbreviation for ${language} is ${shortL}`);
+let name = 'title case';
+
+console.log(name.replace('title case', 'Title Case'));
+
+
